be95b7516b0ca5bcaca8bde5d6670bb98d3abafb
NicolasBeckerich <beckerichnicolas.com>
2023-09-12 14:00:09 +0100

    Mise en place Mongodb Atlas connexion

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
+// Connexion à MongoDB
+mongoose.connect('mongodb+srv://nicolas:projet7oc@cluster0.r1qtcyc.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+})
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
 
 app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
 
 module.exports = app;
