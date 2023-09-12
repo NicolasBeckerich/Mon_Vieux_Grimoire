09cbbe2160df7827c96a0ee239958a80ac4df455
NicolasBeckerich <beckerichnicolas.com>
2023-09-12 18:29:17 +0100

    Début middleware multer config + routes signup avec modif route user.js et app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 const booksRoutes = require('./routes/books');
+const userRoutes = require('./routes/user');
 
 // Connexion à MongoDB
 mongoose.connect('mongodb+srv://nicolas:projet7oc@cluster0.r1qtcyc.mongodb.net/?retryWrites=true&w=majority', {
@@ -23,4 +24,6 @@ app.use((req, res, next) => {
 
 
 app.use('/api/books', booksRoutes);
+app.use('/api/auth', userRoutes);
+
 module.exports = app;
