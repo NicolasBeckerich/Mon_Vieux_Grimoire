62d7d409a68b1e32e7e47bd2da87fa8ea790356e
NicolasBeckerich <beckerichnicolas.com>
2023-09-12 17:46:55 +0100

    Création autre route getOneBook

--- controllers/books.js
+++ controllers/books.js
@@ -5,3 +5,9 @@ exports.getAllBooks = (req, res, next) => {
     .then(books => res.status(200).json(books))
     .catch(error => res.status(400).json({ error }));
 };
+
+exports.getOneBook = (req, res, next) => {
+    Book.findById(req.params.id)
+        .then(book => res.status(200).json(book))
+        .catch(error => res.status(404).json({ error }));
+};
