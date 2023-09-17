4b77122ae168ab20fc4661b6657c139a499bf331
NicolasBeckerich <beckerichnicolas.com>
2023-09-17 20:21:48 +0100

    Modification user.js controllers

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');  
 const jwt = require('jsonwebtoken');  
-const User = require('../models/user');
+const User = require('../models/User');
 
 ///////////////////////////// Inscrire un nouvel utilisateur //////////////////////////////
 exports.signup = (req, res, next) => {
